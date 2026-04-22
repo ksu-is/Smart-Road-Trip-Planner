4f853d9724830faad539d44e337a36b91c75907d
Daniela Orta Rueda <dortarue@students.kennesaw.edu>
2026-04-22 18:00:48 -0400

    Added Best Route Feature
    This feature allows user to input route choices taking into consideration which highway and miles. It will then tell you the best route. I would've liked this to be better developed in the sense where the user would input where the destination is and the stops they have to make (gas, food, dumping of grey/refilling fresh water, etc) but I am just a beginner and am doing what I can.

--- main.py
+++ main.py
@@ -50,10 +50,39 @@ def split_cost_between_travelers(total_cost):
 
     print(f"Cost per person for the trip: ${cost_per_person:.2f}")
 
+# Best Route Options (Beginner Friendly, would like if this was better developed and more efficient for user)
+
+def find_best_route():
+    destination = input("Enter your trip destination: ")
+
+    route1_name = input("Enter name of route option 1: ")
+    route1_miles = float(input(f"Enter the miles for route 1: "))
+
+    route2_name = input("Enter name of route option 2: ")
+    route2_miles = float(input(f"Enter the miles for route 2: "))
+
+    route3_name = input("Enter name of route option 3: ")
+    route3_miles = float(input(f"Enter the miles for route 3: "))
+
+    shortest_miles= min(route1_miles, route2_miles, route3_miles)
+
+    if shortest_miles == route1_miles:
+        best_route = route1_name
+    elif shortest_miles == route2_miles:
+        best_route = route2_name
+    else:
+        best_route = route3_name
+    
+    print(f"The best route to {destination}: {best_route} with {shortest_miles} miles.")
+ 
+
 # Main Page
 
 def main():
     print("Welcome to the Smart Road Trip Planner- Let's plan your trip!")
+
+    find_best_route()
+
     fuel = calculate_fuel_cost()
     lodging = calculate_lodging_cost()
     food = calculate_food_cost()
